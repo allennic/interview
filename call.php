410b940dcde4cd82ca717a5882c8f87ee0f2d367
allennic <huangbaoxun@gmail.com>
2017-03-27 23:13:45 +0800

    Update call.php

--- call.php
+++ call.php
@@ -1,7 +1,7 @@
 <?php
 require('test.php');
 
-$arr = array('a' => 'A', 'b' => 'B');
+$arr = array('a' => 'A', 'b' => 'B', '3'=>'C');
 $test = New Test();
 $test::retain_key_shuffle($arr);
 var_export($arr);
